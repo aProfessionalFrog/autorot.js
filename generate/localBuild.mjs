3f77514fdf0bacd048abc920b304f21bf31a0a08
aProfessionalFrog <nikoh8408@gmail.com>
2024-05-07 19:16:41 -0400

    added prompt generator

--- generate/localBuild.mjs
+++ generate/localBuild.mjs
@@ -3,8 +3,15 @@ import path from 'path';
 import { exec } from 'child_process';
 import { topics } from './topics.mjs';
 import { rm, mkdir, unlink } from 'fs/promises';
+import Groq from 'groq-sdk/index.mjs';
 
 export const PROCESS_ID = 0;
+
+
+const groq = new Groq({
+	apiKey: process.env.GROQ_API_KEY,
+});
+
 
 async function cleanupResources() {
 	try {
@@ -28,32 +35,47 @@ const agents = [
 	'BEN_SHAPIRO',
 	'JORDAN_PETERSON',
 	'JOE_ROGAN',
-	'DONALD_TRUMP',
+	// 'DONALD_TRUMP',
 	// 'RICK_SANCHEZ',
 ];
+
+const backgrounds = [
+	'MINECRAFT',
+	'GTA',
+
+]
 
 const local = true;
 
 async function main() {
-	const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+	/* const videoTopic = await fetch('http://127.0.0.1:11434/api/generate', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: {
+			'model': 'qwen:0.5b',
+			'prompt': 'Write one topic that could be talked about for one minute in the form of a JSON object, in the format "topic": "the idea that you have"',
+			'stream': false
+		}
+	}); */
+	if (topics.length == 0) {
+		await generateTopics();
+	}
+	const videoTopic = topics[topics.pop()];
 	let agentAIndex = Math.floor(Math.random() * agents.length);
 	let agentBIndex;
-
 	do {
 		agentBIndex = Math.floor(Math.random() * agents.length);
 	} while (agentAIndex === agentBIndex);
+	const agentA = agents[agentAIndex];
+	const agentB = agents[agentBIndex];
 
-	// CHANGE THIS VALUE FOR YOUR CHOICE OF AGENTS
-	const agentA = agents[0];
-	const agentB = agents[4];
-
-	// CHANGE THIS VALUE FOR A CUSTOM VIDEO TOPIC
-	const videoTopic = 'Proximal Policy Optimization';
 	const aiGeneratedImages = true;
 	const fps = 60;
 	const duration = 1; //minute
 	//MINECRAFT or TRUCK or GTA
-	const background = 'TRUCK';
+	const background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 	const music = 'NONE';
 	const cleanSrt = true;
 
@@ -80,6 +102,30 @@ async function main() {
 		console.error(`stderr: ${stderr}`);
 
 		cleanupResources();
+	});
+}
+
+async function generateTopics() {
+	const completion = await groq.chat.completions.create({
+		messages: [
+			{
+				role: 'system',
+				content: 'Generate a JSON array of completely random ideas for interesting educational conversation topics.',
+			}
+		],
+		response_format: { type: 'json_object' },
+		model: 'llama3-70b-8192',
+		temperature: 0.5,
+		max_tokens: 4096,
+		top_p: 1,
+		stop: null,
+		stream: false,
+	});
+
+	const content = completion.choices[0]?.message?.content || '';
+	console.log(content);
+	completion.choices[0]?.message?.content.forEach(element => {
+		topics.push(element);
 	});
 }
 
